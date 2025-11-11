206c1d118aeed2ac7ead8e28e137915888bf04f1
Kimbergoldess <kimbergoldess@gmail.com>
2025-11-11 21:12:36 +0100

    Create score_evaluation.py
    Chained conditionals (if/elif/else)

---
+++ Sprint_2/score_evaluation.py
@@ -0,0 +1,10 @@
+points = int(input("Insert your points "))
+
+if points >= 90:
+    print("Your vote is A")
+elif points >= 80:
+    print("Your vote is B")
+elif points >= 70:
+    print("Your vote is C")
+else:
+    print("Your vote is F")
